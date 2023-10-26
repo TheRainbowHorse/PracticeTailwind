d5f58d922c8f1bb1ca301d135361d95f76537dd4
TheRainbowHorse <therainbowhorse3@gmail.com>
2023-10-26 21:25:42 +0300

    Tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Students from './components/Students.vue'
+</script>
+
+<template>
+  <Students />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Students.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+const students = ref([]);
+
+onMounted(() => {
+  axios.get('http://34.82.81.113:3000/students')
+  .then((response) => {
+    students.value = response.data;
+  })
+});
+</script>
+
+<template>
+  <table class="w-full border-3 text-left">
+    <thead class="border-b bg-red-300">
+      <tr>
+        <th>Name</th>
+        <th>Group</th>
+      </tr>
+    </thead>
+    <tbody class="bg-yellow-200">
+      <tr class="border-2" v-for="st in students" v-bind:key="st._id">
+        <td class="px-6 py-4">{{st.name}}</td>
+        <td>{{st.group}}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+</style>
